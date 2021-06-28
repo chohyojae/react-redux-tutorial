027ca0ff2d1a7638b89587f5942093a17da42050
hjcho <hjcho@seegene.com>
2021-06-28 11:18:17 +0900

    implement CounterContainer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Counter from './components/Counter';
 import Todos from './components/Todos';
+import CounterContainer from './containers/CounterContainer';
 
 const App = () => {
   return (
     <>
-      <Counter number={0} />
+      <CounterContainer />
       <hr />
       <Todos />
     </>
