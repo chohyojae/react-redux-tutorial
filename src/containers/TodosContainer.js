998df7070109b9b40efbd2bc4ae11cfad726f03a
hjcho <hjcho@seegene.com>
2021-06-29 09:40:53 +0900

    use Hooks in containers/TodosContainers.js

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,29 +1,33 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { changeInput, insert, toggle, remove } from '../modules/todos';
 import Todos from '../components/Todos';
 
-const TodosContainer = ({
-  input, //input 텍스트박스에 입력되는 값
-  todos, //할 일 목록을 담은 객체
-  changeInput,
-  insert,
-  toggle,
-  remove,
-}) => {
+const TodosContainer = () => {
+  const { input, todos } = useSelector(({ todos }) => ({
+    input: todos.input,
+    todos: todos.todos,
+  }));
+
+  const dispatch = useDispatch();
+  const onChangeInput = useCallback(
+    (input) => dispatch(changeInput(input)),
+    [dispatch],
+  );
+  const onInsert = useCallback((text) => dispatch(insert(text)), [dispatch]);
+  const onToggle = useCallback((id) => dispatch(toggle(id)), [dispatch]);
+  const onRemove = useCallback((id) => dispatch(remove(id)), [dispatch]);
+
   return (
     <Todos
       input={input}
       todos={todos}
-      onChangeInput={changeInput}
-      onInsert={insert}
-      onToggle={toggle}
-      onRemove={remove}
+      onChangeInput={onChangeInput}
+      onInsert={onInsert}
+      onToggle={onToggle}
+      onRemove={onRemove}
     />
   );
 };
 
-export default connect(
-  ({ todos }) => ({ input: todos.input, todos: todos.todos }),
-  { changeInput, insert, toggle, remove },
-)(TodosContainer);
+export default TodosContainer;
